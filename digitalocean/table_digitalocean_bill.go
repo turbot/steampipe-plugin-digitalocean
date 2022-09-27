95b3e8fe35353eec8b42e87d631e81a06718a73a
Ved misra <47312748+misraved@users.noreply.github.com>
2022-09-27 19:29:28 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.7 and Go version 1.19 (#63)

--- digitalocean/table_digitalocean_bill.go
+++ digitalocean/table_digitalocean_bill.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/digitalocean/godo"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
 
 func tableDigitalOceanBill(ctx context.Context) *plugin.Table {
