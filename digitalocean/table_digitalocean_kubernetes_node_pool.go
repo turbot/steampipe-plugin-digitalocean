172e6c54c3961f8a888d618be85973390e550713
ParthaI <47887552+ParthaI@users.noreply.github.com>
2023-01-25 14:35:23 +0530

    Add table digitalocean_kubernetes_node_pool closes #67 (#71)
    Co-authored-by: Karan Popat <karan@turbot.com>
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

---
+++ digitalocean/table_digitalocean_kubernetes_node_pool.go
@@ -0,0 +1,202 @@
+package digitalocean
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/digitalocean/godo"
+
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableDigitalOceanKubernetesNodePool(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "digitalocean_kubernetes_node_pool",
+		Description: "DigitalOcean Kubernetes Node Pool",
+		List: &plugin.ListConfig{
+			ParentHydrate: listKubernetesClusters,
+			Hydrate:       listKubernetesNodePools,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.AllColumns([]string{"id", "cluster_id"}),
+			Hydrate:    getKubernetesNodePool,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "id",
+				Type:        proto.ColumnType_STRING,
+				Description: "The unique universal identifier of this node pool.",
+				Transform:   transform.FromField("ID"),
+			},
+			{
+				Name:        "cluster_id",
+				Type:        proto.ColumnType_STRING,
+				Description: "The unique universal identifier of the associated cluster.",
+				Transform:   transform.FromField("ClusterID"),
+			},
+			{
+				Name:        "name",
+				Type:        proto.ColumnType_STRING,
+				Description: "The globally unique human-readable name for the node pool.",
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "auto_scale",
+				Type:        proto.ColumnType_BOOL,
+				Description: "A boolean value indicating whether the node pool has autoscaling enabled.",
+				Transform:   transform.FromField("AutoScale"),
+			},
+			{
+				Name:        "count",
+				Type:        proto.ColumnType_INT,
+				Description: "The number of nodes in the node pool.",
+				Transform:   transform.FromField("Count"),
+			},
+			{
+				Name:        "max_nodes",
+				Type:        proto.ColumnType_INT,
+				Description: "The maximum number of nodes allowed in the node pool.",
+				Transform:   transform.FromField("MaxNodes"),
+			},
+			{
+				Name:        "min_nodes",
+				Type:        proto.ColumnType_INT,
+				Description: "The minimum number of nodes allowed in the node pool.",
+				Transform:   transform.FromField("MinNodes"),
+			},
+			{
+				Name:        "size",
+				Type:        proto.ColumnType_STRING,
+				Description: "The size of the node pool.",
+				Transform:   transform.FromField("Size"),
+			},
+			{
+				Name:        "urn",
+				Type:        proto.ColumnType_STRING,
+				Description: "The uniform resource name (URN) for the node pool.",
+				Transform:   transform.FromValue().Transform(nodePoolToURN),
+			},
+
+			{
+				Name:        "labels",
+				Type:        proto.ColumnType_JSON,
+				Description: "The labels for the node pool.",
+				Transform:   transform.FromField("Labels"),
+			},
+			{
+				Name:        "nodes",
+				Type:        proto.ColumnType_JSON,
+				Description: "The nodes available in the node pool.",
+				Transform:   transform.FromField("Nodes"),
+			},
+			{
+				Name:        "taints",
+				Type:        proto.ColumnType_JSON,
+				Description: "The taints of the node pool.",
+				Transform:   transform.FromField("Taints"),
+			},
+
+			// Steampipe standard columns
+			{
+				Name:        "tags",
+				Type:        proto.ColumnType_JSON,
+				Description: resourceInterfaceDescription("tags"),
+				Transform:   transform.FromField("Tags").Transform(labelsToTagsMap),
+			},
+			{
+				Name:        "title",
+				Type:        proto.ColumnType_STRING,
+				Description: resourceInterfaceDescription("title"),
+				Transform:   transform.FromField("Name"),
+			},
+			{
+				Name:        "akas",
+				Type:        proto.ColumnType_JSON,
+				Description: resourceInterfaceDescription("akas"),
+				Transform:   transform.FromValue().Transform(nodePoolToURN).Transform(ensureStringArray),
+			},
+		},
+	}
+}
+
+type KubernetesNodePoolInfo struct {
+	godo.KubernetesNodePool
+	ClusterID string
+}
+
+//// LIST FUNCTION
+
+func listKubernetesNodePools(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	cluster := h.Item.(*godo.KubernetesCluster)
+
+	conn, err := connect(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("digitalocean_kubernetes_node_pool.listKubernetesNodePools", "connection_error", err)
+		return nil, err
+	}
+	opts := &godo.ListOptions{
+		Page:    1,
+		PerPage: 100,
+	}
+	for {
+		nodePools, resp, err := conn.Kubernetes.ListNodePools(ctx, cluster.ID, opts)
+		if err != nil {
+			plugin.Logger(ctx).Error("digitalocean_kubernetes_node_pool.listKubernetesNodePools", "query_error", err, "id", cluster.ID)
+			return nil, err
+		}
+		for _, nodePool := range nodePools {
+			d.StreamListItem(ctx, &KubernetesNodePoolInfo{*nodePool, cluster.ID})
+		}
+		// if we are at the last page, break out the for loop
+		if resp.Links == nil || resp.Links.IsLastPage() {
+			break
+		}
+		page, err := resp.Links.CurrentPage()
+		if err != nil {
+			return nil, err
+		}
+		// set the page we want for the next request
+		opts.Page = page + 1
+	}
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+
+func getKubernetesNodePool(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	id := d.KeyColumnQuals["id"].GetStringValue()
+	clusterId := d.KeyColumnQuals["cluster_id"].GetStringValue()
+
+	// Handle empty id
+	if id == "" || clusterId == "" {
+		return nil, nil
+	}
+
+	conn, err := connect(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("digitalocean_kubernetes_node_pool.getKubernetesNodePool", "connection_error", err)
+		return nil, err
+	}
+
+	result, resp, err := conn.Kubernetes.GetNodePool(ctx, clusterId, id)
+	if err != nil {
+		if strings.Contains(err.Error(), ": 404") {
+			return nil, nil
+		}
+		plugin.Logger(ctx).Error("digitalocean_kubernetes_node_pool.getKubernetesNodePool", "query_error", err, "resp", resp)
+		return nil, err
+	}
+	return &KubernetesNodePoolInfo{*result, clusterId}, nil
+}
+
+//// TRANSFORM FUNCTION
+
+func nodePoolToURN(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	i := *d.Value.(*KubernetesNodePoolInfo)
+	return fmt.Sprintf("do:kubernetesNodePool:%s", i.ID), nil
+}
---
+++ docs/tables/digitalocean_kubernetes_node_pool.md
@@ -0,0 +1,94 @@
+# Table: digitalocean_kubernetes_node_pool
+
+A node pool is a group of nodes with the same configuration within a cluster.
+
+Each node within the node pool has a Kubernetes node label which is the node pool’s name. The node pool may be resized up or down to accommodate workloads.
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  id,
+  name,
+  cluster_id
+  auto_scale
+from
+  digitalocean_kubernetes_node_pool;
+```
+
+### List node pools with autoscaling disabled
+
+```sql
+select
+  id,
+  name,
+  cluster_id,
+  auto_scale
+from
+  digitalocean_kubernetes_node_pool
+where
+  not auto_scale;
+```
+
+### Count numbers of nodes per node pool
+
+```sql
+select
+  id,
+  name,
+  cluster_id,
+  count as node_count
+from
+  digitalocean_kubernetes_node_pool;
+```
+
+### Get node details of node pools
+
+```sql
+select
+  p.id,
+  p.name,
+  n ->> 'created_at' as node_created_at,
+  n ->> 'droplet_id' as node_droplet_id,
+  n ->> 'id' as node_id,
+  n ->> 'name' as node_name,
+  n ->> 'status' as node_status,
+  n ->> 'updated_at' as node_updated_at
+from
+  digitalocean_kubernetes_node_pool as p,
+  jsonb_array_elements(nodes) as n;
+```
+
+### Get the top five node pools with the most nodes
+
+```sql
+select
+  id,
+  name,
+  cluster_id,
+  max_nodes
+from
+  digitalocean_kubernetes_node_pool
+order by
+  max_nodes desc
+limit 5;
+```
+
+### Get cluster details for the node pools
+
+```sql
+select
+  n.id as node_pool_id,
+  n.name,
+  n.cluster_id,
+  c.status as cluster_status,
+  c.cluster_subnet,
+  c.endpoint as cluster_endpoint
+from
+  digitalocean_kubernetes_node_pool as n,
+  digitalocean_kubernetes_cluster as c
+where
+  c.id = cluster_id;
+````
